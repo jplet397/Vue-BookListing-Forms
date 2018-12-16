d8bca7aafc39564d5a370fca8a216c42d1d20d5b
Joris Plettinx <Joris.Plettinx@realdolmen.com>
2018-12-16 17:57:40 +0100

    module2

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,27 +1,47 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div>
+      <label for="finishedReading">
+        Finished Reading
+        <input v-model="bookData.finishedReading" type="checkbox"/>
+      </label>
+    </div>
+    <div>
+      <label for="borrowed">
+        borrowed
+        <input v-model="bookData.ownership" value="borrowed" type="radio"/>
+      </label>
+      <label for="bought">
+        bought
+        <input v-model="bookData.ownership" value="bought" type="radio"/>
+      </label>
+    </div>
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>
 
 <script>
-export default {
-  name: "BookForm",
-  props: ["books"],
-  data() {
-    return {
-      bookTitle: "",
-      bookAuthor: ""
-    };
-  },
-  methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+  export default {
+    name: "BookForm",
+    props: ["books"],
+    data() {
+      return {
+        bookData: {
+          bookTitle: "",
+          bookAuthor: "",
+          finishedReading: false,
+          ownership: []
+        }
+      };
+    },
+    methods: {
+      bookSubmit(bookData) {
+        this.$emit("addBook", bookData.bookTitle, bookData.bookAuthor);
+      }
     }
-  }
-};
+  };
 </script>
 
 <style>
